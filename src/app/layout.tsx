f103827dba30eade59efe0e04ab375a3eaaaa52a
jvyden <jvyden@jvyden.xyz>
2024-01-10 17:46:38 -0500

    Basic styling, navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next'
 import {Rubik} from 'next/font/google'
 import './globals.css'
 import {ReactNode} from "react";
+import Link from "next/link";
 
 const font = Rubik({subsets: ['latin']})
 
@@ -10,12 +11,35 @@ export const metadata: Metadata = {
     description: 'A next-generation frontend for Refresh.',
 }
 
+function NavBarItem({href, title}: {href: string, title: string}) {
+    return (
+        <Link href={href}>{title}</Link>
+    )
+}
+
+function NavBar() {
+    return (
+        <nav className="flex gap-x-1">
+            <NavBarItem href="/" title="Home"></NavBarItem>
+            <NavBarItem href="/asf" title="asf"></NavBarItem>
+        </nav>
+    );
+}
+
 export default function RootLayout({children,}: {
     children: ReactNode
 }) {
     return (
         <html lang="en">
-        <body className={font.className}>{children}</body>
+        <body className={font.className + " bg-background text-foreground m-0 h-full"}>
+        <main>
+            <div className="flex">
+                <h1 className="font-bold mr-1">CosmosFE Prototype</h1>
+                <NavBar></NavBar>
+            </div>
+            {children}
+        </main>
+        </body>
         </html>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,8 @@ const requester: Requester = new Requester();
 export default async function Home() {
     const user = await requester.makeJsonRequest<any>("GET", "/users/name/jvyden420");
     return (
-        <main className="">
-            <h1>CosmosFE Prototype</h1>
-            <pre className="whitespace-pre-line">
-                {JSON.stringify(user)}
-            </pre>
-        </main>
+        <pre className="whitespace-pre-line">
+            {JSON.stringify(user)}
+        </pre>
     )
 }
